79cd5500571fa1e09f522e2e6121c0b7fc4ceaea
Brin Brin <z01574@bkkprep.ac.th>
2020-07-13 09:30:41 +0000

    added pairsfour.cpp

---
+++ TOI (Thailand Olympiad of Informatics ) /2014 - toi 10/pairsoffour.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+int k   ; 
+
+char a[1001] ; 
+
+int dp[1001][1001] ; 
+
+
+int main(){
+
+  cin >> k ; 
+
+  for(int i=0;i<k;i++){
+    scanf (" %c", &a[i] ) ; 
+  }
+ 
+
+  for (int l = 1 ; l < k; l++){
+
+    for ( int j = 0 ; j < k - l   ; j ++){
+
+      int en = j + l ; 
+
+      int val = INT_MIN ; 
+      
+      if(a[j] == a[en] && j < en-1){
+        val = 1 + dp[j+1][en-1] ; 
+        // cout<<"yay"<<endl ; 
+      }
+
+      for(int i = j ; i <en ; i++){
+
+        val = max( val , dp[j][i] + dp[i+1][en] ) ; 
+
+      }
+
+      // cout<< j <<" "<<en <<" "<< val<<endl ; 
+
+      dp[j][en] = val ; 
+
+    }
+
+  }
+
+  cout << dp[0][k-1] ; 
+
+}
